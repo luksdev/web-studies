48c5f333ab7ea95529337ce3a768f3cc697a1a04
Roger Luiz <rogerluiz010@gmail.com>
2020-11-06 09:22:01 -0300

    Update main.js

--- get-geolocation/main.js
+++ get-geolocation/main.js
@@ -1,29 +1,28 @@
-window.addEventListener( 'load' , getLocation )
-
-function getLocation () {
-  if ( navigator.geolocation ) {
-    navigator.geolocation.getCurrentPosition( createMap , showError, { enableHighAccuracy: true, maximumAge: 1000, timeout: 60000 } )
-  } else { 
-    console.log( 'A localização geográfica não é suportada por este navegador' )
-  }
+const start = () => {
+  navigator.geolocation ? getLocation() : console.log( 'A localização geográfica não é suportada por este navegador' )
 }
 
-function createMap ( position ) {
+const getLocation = () => {
+  navigator.geolocation
+  .getCurrentPosition( renderMap , showError, { enableHighAccuracy: true, maximumAge: 1000, timeout: 60000 } )
+}
+
+const createMap = ( latitude , longitude ) => {
+  let map = L.map('mapid').setView([ latitude , longitude ] , 18 )
+  L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' ).addTo( map )
+  L.marker( [ latitude , longitude ] ).addTo( map )
+}
+
+const renderMap = position => {
   const UserPosition = {
     Latitude: position.coords.latitude,
     Longitude: position.coords.longitude
   }
 
-  // console.log( `Latitude: ${UserPosition.Latitude}` )
-  // console.log( `Longitude: ${UserPosition.Longitude}` )
-
-  let map = L.map('mapid').setView([UserPosition.Latitude, UserPosition.Longitude], 18)
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-  L.marker([UserPosition.Latitude, UserPosition.Longitude]).addTo(map)
-
+  createMap( UserPosition.Latitude, UserPosition.Longitude )
 }
 
-function showError ( error ) { 
+const showError = error => { 
   switch ( error.code ) {
     case error.PERMISSION_DENIED:
       console.log( 'Usuário negou a solicitação de geolocalização.' )
@@ -42,3 +41,5 @@ function showError ( error ) {
       break
   }
 }
+
+window.addEventListener( 'load' , start )
