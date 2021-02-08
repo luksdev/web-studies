568f3648494517b8ec931b627ef8b1e1f7fa9b41
Roger Luiz <rogerluiz010@gmail.com>
2021-02-08 16:45:01 -0300

    added css tricks

---
+++ loader_square/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Loader Square</title>
+    <link href="css/style1.css" rel="stylesheet">
+</head>
+
+<body>
+    <section>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </section>
+</body>
+
+</html>
